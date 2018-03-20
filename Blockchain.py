0919feedc2d36c8f657acb3409f90317e5e53d9c
Victor Phung <victor.phung@gmail.com>
2018-03-20 22:46:30 +1100

    added consensus algorithm to the blockchain

--- Blockchain.py
+++ Blockchain.py
@@ -5,14 +5,91 @@ from textwrap import dedent
 from uuid import uuid4
 
 from flask import Flask, jsonify, request
+from urllib.parse import urlparse
+import requests
 
-class Blockchain(object):
-	def __inti__(self):
+class Blockchain:
+	def __init__(self):
 		self.chain = []
 		self.current_transactions = []
+		self.nodes = set()
 
 		# Create the gensis block
 		self.new_block(previous_hash=1, proof=100)
+
+	def register_node(self, address):
+		"""
+		add a new node to the list of nodes
+
+		:param address:	<str> address of node e.g. http://192.168.0.5:5000
+		:return: 		None
+		"""
+
+		parsed_url = urlparse(address)
+		self.nodes.add(parsed_url.netloc)
+
+	def valid_chain(self, chain):
+		"""
+		Determine if a given blockchain is valid
+
+		:param chain:	<list> A blockchain
+		:return:		<bool> True if valid, False otherwise
+		"""
+
+		last_block = chain[0]
+		current_index = 1
+
+		while current_index < len(chain):
+			block = chain[current_index]
+			print(f'{last_block}')
+			print(f'{block}')
+			print("\n------------\n")
+			# Check that the hash of the block is correct
+			if block['previous_hash'] != self.hash(last_block):
+				return False
+
+			# Check that the Proof of Work is correct
+			if not self.valid_proof(last_block['proof'], block['proof']):
+				return False
+
+			last_block = block
+			current_index += 1
+
+		return True
+
+	def resolve_conflict(self):
+		"""
+		This is our Consensus Algorithm, it resolves conflicts
+		by replacing our chain with the longest one in the network
+
+		:return:	<bool> True if our chain was replaced, False otherwise
+		"""	
+
+		neighbours = self.nodes
+		new_chain = None
+
+		# We are only looking for chains longer than ours
+		max_length = len(self.chain)
+
+		# Grab and verify the chains from all nodes in the network
+		for node in neighbours:
+			response = requests.get(f'http://{node}/chain')
+
+			if response.status_code == 200:
+				length = response.json()['length']
+				chain = response.json()['chain']
+
+				# Check if the length is longer and the chain is valid
+				if length > max_length and self.valid_chain(chain):
+					max_length = length
+					new_chain = chain
+		
+		# Replace our chain if we discovered  a new, valid chain longer than ours
+		if new_chain:
+			self.chain = new_chain
+			return True
+		
+		return False
 
 	def new_block(self, proof, previous_hash=None):
 		# Creates a new Block and adds it to the chain
@@ -37,6 +114,7 @@ class Blockchain(object):
 		self.current_transactions = []
 
 		self.chain.append(block)
+		print('added to chain')
 		return block
 
 	def new_transaction(self, sender, recipient, amount):
@@ -119,7 +197,32 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-	return "We'll mine a new Block"
+	# We run the proof of work algorithm to get the next proof...
+	last_block = blockchain.last_block
+	last_proof = last_block['proof']
+	proof = blockchain.proof_of_work(last_proof)
+	
+	# We must receive a reward for finding the proof.
+	# The sender is "0" to signify that this node has mined a new coin
+	blockchain.new_transaction(
+		sender="0",
+		recipient=node_identifier,
+		amount=1,
+	)
+
+	# Forge the new Block by adding it to the chain
+	previous_hash = blockchain.hash(last_block)
+	block = blockchain.new_block(proof, previous_hash)
+
+	response = {
+		'message': 'New Block Forged',
+		'index': block['index'],
+		'transactions': block['transactions'],
+		'proof': block['proof'],
+		'previous_hash': block['previous_hash']
+	}
+
+	return jsonify(response), 200
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
@@ -134,7 +237,7 @@ def new_transaction():
 	index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
 	
 	response = {'message': f'Transaction will be added to Block {index}'}
-	return jsonify(response), 200
+	return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
@@ -143,7 +246,6 @@ def full_chain():
 		'length': len(blockchain.chain),
 	}	
 	return jsonify(response), 200
-
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
