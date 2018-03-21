028e7ed9b04afc3185dda69716eb6e00971c456a
Victor Phung <victor.phung@gmail.com>
2018-03-21 22:29:16 +1100

    added endpoints to register new nodes and resolve longest chain based on consensus

--- Blockchain.py
+++ Blockchain.py
@@ -57,7 +57,7 @@ class Blockchain:
 
 		return True
 
-	def resolve_conflict(self):
+	def resolve_conflicts(self):
 		"""
 		This is our Consensus Algorithm, it resolves conflicts
 		by replacing our chain with the longest one in the network
@@ -247,5 +247,46 @@ def full_chain():
 	}	
 	return jsonify(response), 200
 
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+	values = request.get_json()
+
+	nodes = values.get('nodes')
+	if nodes is None:
+		return "Error: Please supply a valid list of nodes", 400
+	
+	for node in nodes:
+		blockchain.register_node(node)
+	
+	response = {
+		'message': 'New nodes have been added',
+		'total_nodes': list(blockchain.nodes)
+	}
+
+	return jsonify(response), 201
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+	replaced = blockchain.resolve_conflicts()
+
+	if replaced:
+		response = {
+			'message': 'Our chain was replaced',
+			'new_chain': blockchain.chain
+		}
+	else:
+		response = {
+			'message': 'Our chain is authorative',
+			'chain' : blockchain.chain
+		}
+
+	return jsonify(response), 200
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+	from argparse import ArgumentParser
+
+	parser = ArgumentParser()
+	parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+	args = parser.parse_args()
+	port = args.port
+	app.run(host='0.0.0.0', port=port)
